153772df32c7b09905445032f39a22aa3ec40a17
VitalC-3026 <1184649102@qq.com>
2024-06-24 03:24:44 +0000

    trt install

--- examples/cpp_library/build.sh
+++ examples/cpp_library/build.sh
@@ -2,6 +2,7 @@
 
 BUILD_DIR="build"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TENSORRT_LLM_DIR="../../tensorrt_llm/libs"
 
 rm -rf ${BUILD_DIR} && mkdir -p ${BUILD_DIR}
 
@@ -14,10 +15,12 @@ cmake \
 make -j"$(grep -c ^processor /proc/cpuinfo)"
 
 export LD_LIBRARY_PATH="${SCRIPT_DIR}:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${TENSORRT_LLM_DIR}:${LD_LIBRARY_PATH}"
 
 # Test Lib
 echo
 echo "--------------------------------------------------------------------"
+echo "${LD_LIBRARY_PATH}"
 ./trt_llm_plugins_cpp_load_example
 echo "--------------------------------------------------------------------"
 echo
